f556aa72eeede951a4e30dd1c97164f714dcded6
Helen <frontendledy@gmail.com>
2026-01-13 17:06:55 +0300

    new commit

--- scripts/answers.js
+++ scripts/answers.js
@@ -1,5 +1,9 @@
 (function () {
     const Answers = {
+        currentQuestionIndex: 1, // индекс ТЕКУЩЕГО вопроса (полученного с сервера), (но первый у нас будет с индексом -1)
+        showQuestionsElement: null, //вопросы с ответами выбранного теста
+        questions: [], //вопросы выбранного теста
+        answers: [], //ответы на вопросы выбранного теста
         quiz: [],
         init() {
 
@@ -17,7 +21,7 @@
                     } catch (e) {
                         location.href = 'index.html';
                     }
-                    this.startQuiz(); // вызов Ф startQuiz, отобразится первый вопрос
+                    this.showSelectedTest(); // вызов Ф showSelectedTest, отобразится выбранный пользователем тест
                 } else {
                     location.href = 'index.html'; // если статус будет не === 200
                 }
@@ -27,11 +31,47 @@
             }
 
         },
-        startQuiz() { //обработка данных, полученных с сервера - вывод этих данных на страницу html
-            console.log(this.quiz); //увидим что пришло с backend (видим id,name,questions)
+        showSelectedTest() { //обработка данных, полученных с сервера - вывод этих данных на страницу html
+            console.log(this.quiz.questions); //увидим что пришло с backend (видим id,name,questions)
+            document.getElementById('test-passed').innerText = this.quiz.name; //вывод на страницу названия теста
+            this.showQuestionsElement = document.getElementById('answers-list')
 
+            for (let i = 0; i < this.quiz.questions.length; i++) { // с помощью for создадим нужное количество элементов вопросов с ответами
+                const itemElement = document.createElement('div'); // создаем div (answer)
+                itemElement.className = 'answer'; // присваиваем с индексом вопроса
+
+                const itemTitleElement = document.createElement('div'); // создаем div (в answer)
+                itemTitleElement.className = 'common-title answer-title'
+                const question = this.quiz.questions[i]; // создадим переменную с текущим новым вопросом теста (его индекс)
+                itemTitleElement.innerHTML = '<span> Вопрос ' + [i+1] + ':</span> ' + question.question;
+
+                itemElement.appendChild(itemTitleElement);
+
+
+                for (let j = 0; j < question.answers.length; j++) {
+                    const answerElement = document.createElement('div'); //сюда соберем один ответ
+                    answerElement.className = 'test-answer-option';
+
+                    const answersCircleElement = document.createElement('div'); //создадим кружок
+                    answersCircleElement.className = 'circle';
+
+                    const answersTextElement = document.createElement('div'); //подгрузим ответы
+                    answersTextElement.className = 'answer-text';
+                    const answer = question.answers[j];
+                    answersTextElement.innerHTML = answer.answer;
+
+                    answerElement.appendChild(answersCircleElement);
+                    answerElement.appendChild(answersTextElement);
+
+                    itemElement.appendChild(answerElement);
+                }
+
+
+
+                this.showQuestionsElement.appendChild(itemElement); // теперь в html можно удалить вопросы и ответы
+
+            }
         }
-
 
     }
     Answers.init();
